7c6845ac5efa616475cfdc48658c7b7b92623eaa
Juan Molano <molano.juan.2629@eam.edu.co>
2025-04-04 20:51:51 -0500

    CONFIGURADA Y RENOMBRADA CLASE DE TESTS para Main, para que pueda encontrar a main.

--- src/test/java/com/group/gestion_cine/MainTests.java
+++ src/test/java/com/group/gestion_cine/MainTests.java
@@ -1,10 +1,11 @@
 package com.group.gestion_cine;
 
+import app.main.Main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest()
-class GestionCineApplicationTests {
+@SpringBootTest(classes = Main.class)
+class MainTests {
 
 	@Test
 	void contextLoads() {
